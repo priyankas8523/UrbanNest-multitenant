8d3b925bba865798105989a3b3bd35a4ae0cd6b2
priyankas8523 <priyankasarode2001@gmail.com>
2026-03-23 19:44:35 +0530

    Added Property entity and liquibase yaml

--- src/main/java/com/keycloak/demo/entity/PropertyEntity.java
+++ src/main/java/com/keycloak/demo/entity/PropertyEntity.java
@@ -1,17 +1,63 @@
 package com.keycloak.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import com.keycloak.demo.enums.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import jakarta.validation.constraints.Size;
+
+import java.util.UUID;
 
 @Entity
+@Table(name = "PROPERTY")
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PropertyEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
     private Long id;
 
+    private UUID uuid;
+
+    @NotBlank
+    @Size(max = 100)
+    @Column(name = "PROPERTY_NAME")
     private String name;
 
-    private String address;
+    @NotBlank
+    @Size(max = 100)
+    @Column(name = "ADDR1")
+    private String address1;
+
+    @NotBlank
+    @Size(max = 100)
+    @Column(name = "ADDR2")
+    private String address2;
+
+    @NotBlank
+    @Size(max = 100)
+    @Column(name = "CITY")
+    private String city;
+
+    @NotBlank
+    @Size(max = 50)
+    @Column(name = "STATE")
+    private String state;
+
+    @NotBlank
+    @Size(max = 10)
+    @Column(name = "POSTAL_CODE")
+    private String postalCode;
+
+    @Column(name = "STATUS")
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
 }
